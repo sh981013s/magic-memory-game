30f962207d0fa1df47fa85895993c94c276217de
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-20 19:53:22 +0900

    feat: entire card template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Back from './img/cover.png';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: './img/potion-1.png' },
-  { src: './img/ring-1.png' },
-  { src: './img/scroll-1.png' },
-  { src: './img/shield-1.png' },
-  { src: './img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: './img/potion-1.png', matched: false },
+  { src: './img/ring-1.png', matched: false },
+  { src: './img/scroll-1.png', matched: false },
+  { src: './img/shield-1.png', matched: false },
+  { src: './img/sword-1.png', matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -25,7 +27,38 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // handling choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        resetTurn();
+      }
+      console.log(cards);
+    }
+  }, [choiceOne, choiceTwo]);
 
   return (
     <div className="App">
@@ -34,7 +67,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import './SingleCard.css';
 import Back from '../img/cover.png';
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src={Back} alt="card front" />
+        <img
+          className="back"
+          src={Back}
+          onClick={handleClick}
+          alt="card front"
+        />
       </div>
     </div>
   );
